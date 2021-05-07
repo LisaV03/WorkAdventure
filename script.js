94595a0e867a5ed41174eb531a38073145538fbf
Veillat <lisa.veillat@siemens-healthineers.com>
2021-05-07 16:22:03 +0200

    meine Map avec 2 messages2

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-WA.sendChatMessage('Hello world', 'Mr Robot');
+WA.sendChatMessage('Message #1', 'Tutor');
+
+WA.onChatMessage((message => {
+WA.sendChatMessage(message, 'echo');
+}));
+
+WA.sendChatMessage('Message #2', 'Tutor');
